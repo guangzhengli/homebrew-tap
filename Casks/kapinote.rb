1e4d0dfd2d7e55a9b867ff0fd4b2f8b67bbf6a6a
guangzhengli <iguangzhengli@gmail.com>
2026-04-23 21:40:40 +0800

    Update kapinote to v0.2.1

---
+++ Casks/kapinote.rb
@@ -0,0 +1,20 @@
+cask "kapinote" do
+  version "0.2.1"
+  sha256 "ec538bd0ba47377031a2715ed2aa18a7a925ee1579d93718480a869ebb122211"
+
+  url "https://github.com/guangzhengli/kapinote-releases/releases/download/v#{version}/Kapinote_0.2.1_aarch64.dmg",
+      verified: "github.com/guangzhengli/kapinote-releases/"
+  name "Kapinote"
+  desc "Desktop meetings app"
+  homepage "https://github.com/guangzhengli/kapinote-releases"
+
+  depends_on arch: :arm64
+
+  app "Kapinote.app"
+
+  zap trash: [
+    "~/Library/Application Support/Kapinote",
+    "~/Library/Caches/com.guangzhengli.kapinote",
+    "~/Library/Preferences/com.guangzhengli.kapinote.plist",
+  ]
+end
