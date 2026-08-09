cb8d08c61b968c4b2dc8602779f401aedfdedb45
guangzhengli <iguangzhengli@gmail.com>
2026-08-09 21:45:14 +0800

    Update kapinote to v0.9.1

--- Casks/kapinote.rb
+++ Casks/kapinote.rb
@@ -1,8 +1,8 @@
 cask "kapinote" do
-  version "0.9.0"
-  sha256 "e8890c371da472e78e4fb404f8dfa4d8434fa8fe9cc736d5d388be50520fdb46"
+  version "0.9.1"
+  sha256 "90c51b0a5dbe5a4ec3eff365049f6f9ee0593663d2cec96758fbde4be7a30cc7"
 
-  url "https://github.com/guangzhengli/kapinote-releases/releases/download/v#{version}/Kapinote_0.9.0_aarch64.dmg"
+  url "https://github.com/guangzhengli/kapinote-releases/releases/download/v#{version}/Kapinote_0.9.1_aarch64.dmg"
   name "Kapinote"
   desc "Desktop meetings app"
   homepage "https://github.com/guangzhengli/kapinote-releases"
